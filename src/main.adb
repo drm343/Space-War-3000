73c0d1b540aa5576923fdbf2c99aa9c77a7ed685
erlin <drm343@gmail.com>
2017-01-22 00:55:11 +0800

    Modify main for fit new code style.

--- src/main.adb
+++ src/main.adb
@@ -1,23 +1,26 @@
 with Ada.Text_IO;
 with ship;
-with Planet;-- ucdse planet;
+with Planet;
+with Player;
 with Rule;
 
 procedure main is
    my_ship: ship.Ship_Data := ship.is_StarShip;
 
-   my_planet: Planet.Object := Planet.build ("human", Rule.military, 1,
-                                                 3, 1, Rule.starship_power,
-                                                 Rule.less_resource);
-   your_planet: Planet.Object := Planet.build ("enemy", Rule.colony, 1,
-                                                3, 1, Rule.easy_colony,
-                                                Rule.hard_build);
+   my_homeworld   : Planet.Object := Planet.Build ("Earth", 1, 3, 1);
+   your_homeworld : Planet.Object := Planet.Build ("Moon", 1, 3, 1);
+
+   player1 : Player.Object := Player.Build ("human",
+                                            Rule.starship_power,
+                                            Rule.hard_build,
+                                            Rule.military,
+                                            my_homeworld);
 begin
    Ada.Text_IO.Put_Line ("my ship: " & ship.show_me_ship_type (my_ship));
    Ada.Text_IO.Put_Line ("my ship before build: " & Integer'Image (ship.show_me_ship_data (my_ship, ship.build)));
    ship.modify_ship_data (my_ship, ship.build, 1);
    Ada.Text_IO.Put_Line ("my ship after build: " & Integer'Image (ship.show_me_ship_data (my_ship, ship.build)));
 
-   Ada.Text_IO.Put_Line (my_planet.show);
-   Ada.Text_IO.Put_Line (your_planet.show);
+   Ada.Text_IO.Put_Line (my_homeworld.show);
+   Ada.Text_IO.Put_Line (your_homeworld.show);
 end main;
